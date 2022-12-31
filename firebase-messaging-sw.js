8d418038905d462bb665d3c3be5bd55e6bc408fd
kerbong <69619907+kerbong@users.noreply.github.com>
2022-12-31 18:08:50 +0900

    fcm test v1
    메세징 테스트 중.. 버전1

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,19 +1,44 @@
-import { initializeApp } from "firebase/app";
-import { getMessaging } from "firebase/messaging/sw";
-import { onMessage } from "firebase/messaging";
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register(process.env.PUBLIC_URL + "/firebase-messaging-sw.js")
+    .then(function (registration) {
+      console.log("Registration successful, scope is:", registration.scope);
+    })
+    .catch(function (err) {
+      console.log("Service worker registration failed, error:", err);
+    });
+}
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-};
-initializeApp(firebaseConfig);
+self.addEventListener("install", function (e) {
+  console.log("fcm sw install..");
+  self.skipWaiting();
+});
 
-const messaging = getMessaging();
+self.addEventListener("activate", function (e) {
+  console.log("fcm sw activate..");
+});
 
-onMessage(messaging, (payload) => {
-  console.log("Message received. ", payload);
-  // ...
+self.addEventListener("push", function (e) {
+  console.log(e);
+  console.log("push: ", e.data.json());
+  if (!e.data.json()) return;
+
+  const resultData = e.data.json().notification;
+  const notificationTitle = resultData.title;
+  const notificationOptions = {
+    body: resultData.body,
+    icon: resultData.image,
+    tag: resultData.tag,
+    ...resultData,
+  };
+  console.log("push: ", { resultData, notificationTitle, notificationOptions });
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
+
+self.addEventListener("notificationclick", function (event) {
+  console.log("notification click");
+  const url = "/";
+  event.notification.close();
+  event.waitUntil(clients.openWindow(url));
 });
