76e7429c102f7c0dfe3553198b8918308eda4d47
kerbong <kerbong@gmail.com>
2023-09-01 02:16:49 +0900

    school find api func done

--- src/components/Classgame/Crawling/HwpControl.js
+++ src/components/Classgame/Crawling/HwpControl.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { dbService, storageService } from "../../../fbase";
 import { ref, uploadBytes, getBlob, getDownloadURL } from "firebase/storage";
+
+import Swal from "sweetalert2";
 import Docxtemplater from "docxtemplater";
 import PizZip from "pizzip";
 import saveAs from "file-saver";
@@ -8,6 +10,10 @@ import saveAs from "file-saver";
 const HwpControl = (props) => {
   const [file, setFile] = useState(null);
   const [uploadDone, setUploadDone] = useState(false);
+  const [scLoSelect, setScLoSelect] = useState("");
+  const [scName, setScName] = useState("");
+  const [scResult, setScResult] = useState([]);
+  const [school, setSchool] = useState("");
   const [templateData, setTemplateData] = useState([
     {
       name: "홍길동",
@@ -15,16 +21,48 @@ const HwpControl = (props) => {
     },
   ]);
 
+  // 학교명 입력하면면 api로 받아오기
+  const findSchool = (e) => {
+    e.preventDefault();
+    if (scName === "") return;
+    let filter = "&SCHUL_NM=" + scName + "초등학교";
+    getSchoolDatas(filter);
+  };
+
   useEffect(() => {
     if (file) return;
     // 화면 로딩 시에 파일 다운로드
     downloadFile();
   }, [uploadDone]);
 
-  // useEffect(() => {
-  //   if (!file) return;
-  //   // 화면 로딩 시에 파일 다운로드
-  // }, [file]);
+  useEffect(() => {
+    if (school === "") return;
+    // 화면 로딩 시에 파일 다운로드
+    console.log(school);
+  }, [school]);
+
+  // 학교정보 api 다운로드
+  const getSchoolDatas = (filter) => {
+    let baseUrl =
+      "https://open.neis.go.kr/hub/schoolInfo?Type=json&pIndex=1&pSize=1000&SCHUL_KND_SC_NM=초등학교";
+    let key = "&KEY=" + process.env.REACT_APP_NEIS_OPEN_API;
+
+    const fetchData = async () => {
+      const res = await fetch(baseUrl + key + filter);
+      const result = res.json();
+      return result;
+    };
+
+    fetchData().then((res) => {
+      if (res?.schoolInfo) {
+        setScResult(res?.schoolInfo?.[1]?.row);
+        console.log(res?.schoolInfo?.[1]?.row);
+      } else {
+        Swal.fire("검색오류", "학교명을 확인해주세요", "info");
+        setScResult([]);
+      }
+    });
+  };
 
   // Firebase Storage에 파일 업로드
   const uploadFile = async (file) => {
@@ -135,13 +173,38 @@ const HwpControl = (props) => {
   };
 
   const printFile = async (data) => {
-    const output = await makeFileAdapted(data);
-    const fileURL = URL.createObjectURL(output);
-    const printWindow = window.open(fileURL);
-    printWindow.onload = function () {
-      printWindow.print();
-      URL.revokeObjectURL(fileURL);
-    };
+    try {
+      const output = await makeFileAdapted(data);
+      const fileURL = URL.createObjectURL(output);
+
+      const iframe = document.createElement("iframe");
+      iframe.style.display = "none";
+      iframe.src = fileURL;
+
+      document.body.appendChild(iframe);
+
+      iframe.onload = function () {
+        if (!iframe.contentWindow || !iframe.contentWindow.print) {
+          // 프린트 기능이 지원되지 않는 경우
+          console.log("프린트를 지원하지 않습니다.");
+          Swal.fire(
+            "인쇄 실패",
+            "인쇄를 지원하지 않는 브라우저 환경입니다.",
+            "error"
+          );
+          // 여기에서 프린트 연결 방법을 안내하는 UI를 표시할 수 있습니다.
+        } else {
+          // 프린트 기능이 지원되는 경우
+          setTimeout(function () {
+            iframe.contentWindow.print();
+            URL.revokeObjectURL(fileURL);
+            document.body.removeChild(iframe);
+          }, 1500); // 인쇄가 시작되기 전에 충분한 시간을 기다립니다.
+        }
+      };
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -157,6 +220,34 @@ const HwpControl = (props) => {
           <button onClick={() => printFile(temp_data)}>인쇄</button>
         </div>
       ))}
+
+      {/* 학교찾기 */}
+      <form onSubmit={(e) => findSchool(e)}>
+        <input
+          type="text"
+          placeholder="학교명 입력(초등학교 제외 / 가나초의 경우 가나)"
+          onChange={(e) => setScName(e.target.value)}
+          value={scName}
+        />{" "}
+        <span>초등학교</span>
+        <button onClick={findSchool}>검색</button>
+      </form>
+
+      {/* 찾으면 학교보여주기 */}
+      <ul>
+        {scResult?.map((sc, index) => (
+          <li
+            key={index}
+            onClick={() => {
+              let name =
+                sc.JU_ORG_NM?.split("교육지원청")?.[0] + "*" + sc.SCHUL_NM;
+              setSchool(name);
+            }}
+          >
+            위치 : {sc.ORG_RDNMA}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
