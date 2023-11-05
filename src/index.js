4042a967e32cc8317be1487e923cb2060403e1c5
kerbong <kerbong@gmail.com>
2023-11-05 23:00:57 +0900

    ga4 up

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
+import ReactGA from "react-ga4";
+
+ReactGA.initialize(process.env.REACT_APP_MEASUREMENT_ID);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter basename={process.env.PUBLIC_URL}>
